c53316665e6964ff7da30a7aed507d678589d6fa
sailendrachettri <sailendra9083@gmail.com>
2023-06-23 10:05:30 +0530

    user auth created - lec9

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,31 @@
 // import mongoose, { model } from "mongoose"
-const { mongoose, model } = require('mongoose')
+const { mongoose } = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userModel = mongoose.Schema(
     {
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        pic: { type: String, default: process.env.PUBLIC_URL + ("/imgs/avatar.png") }
+        pic: { type: String, default: "backend/imgs/avatar.png" }
     },
     {
         timeStamp: true
     }
 )
+
+userModel.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userModel.pre('save', async function (next) {
+    if (!this.isModified) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userModel)
 module.exports = User
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,16 @@ const dotenv = require('dotenv')
 const { chats } = require('./data')
 const conncetDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const cors = require("cors");
 
 
 const app = express()
+app.use(cors());
 dotenv.config()
 conncetDB()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 6000
 
 app.use(express.json()) // to accept json data
 
@@ -18,5 +21,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/user/', userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server started at http://localhost:${PORT}`))
